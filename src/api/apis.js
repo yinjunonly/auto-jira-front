8436e5829648a881d0e4f9386b4115fcd2119051
yinjun <yinjunonly>
2020-12-10 00:24:57 +0800

    feat: 新建issue开发完成

--- src/api/apis.js
+++ src/api/apis.js
@@ -13,3 +13,5 @@ import qs from "querystring";
 
 /** 获取issue创建信息 */
 export const getIssueInfo = params => httpGet('/jira/issue/info', params);
+/** 创建issue并log工时 */
+export const createIssueLog = params => httpPost('/jira/issue/automation', params);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
       centerCollapsed
     >
       <template #left>
-        <h2>XXXX</h2>
+        <h2>Quick Jira</h2>
       </template>
       <template #right>
         <h2>{{ issueData.assignee }}</h2>
@@ -17,22 +17,51 @@
       <vs-table striped>
         <template #thead>
           <vs-tr>
-            <vs-th> 项目 </vs-th>
-            <vs-th> 问题类型 </vs-th>
-            <vs-th> 标题 </vs-th>
-            <vs-th> 分类 </vs-th>
-            <vs-th> 子分类 </vs-th>
-            <vs-th> 日期 </vs-th>
-            <vs-th> 工时(hours) </vs-th>
-            <vs-th> 操作 </vs-th>
+            <vs-th>ID </vs-th>
+            <vs-th>项目 </vs-th>
+            <vs-th>问题类型 </vs-th>
+            <vs-th>标题 </vs-th>
+            <vs-th>分类 </vs-th>
+            <vs-th>子分类 </vs-th>
+            <vs-th>日期 </vs-th>
+            <vs-th>工时(hours) </vs-th>
+            <vs-th>操作 </vs-th>
           </vs-tr>
         </template>
         <template #tbody>
           <vs-tr :key="i" v-for="(tr, i) in workLogData" :data="tr">
             <vs-td>
-              <vs-select placeholder="Select" v-model="tr.projectId">
+              {{ i + 1 }}
+            </vs-td>
+            <vs-td>
+              <vs-select
+                @change="projectChange(tr, i)"
+                filter
+                style="width: 200px"
+                placeholder="请选择"
+                v-model="tr.projectId"
+              >
                 <vs-option
+                  :key="item.id"
                   v-for="item in issueData.projects"
+                  :label="item.name"
+                  :value="item.id"
+                >
+                  {{ item.name }}
+                </vs-option>
+              </vs-select>
+            </vs-td>
+            <vs-td>
+              <vs-select
+                v-if="tr.typeSelectIsShow"
+                placeholder="请选择"
+                style="width: 200px"
+                v-model="tr.issueTypeId"
+                :disabled="!tr.issueTypes"
+                @change="issueTypeChange(tr, i)"
+              >
+                <vs-option
+                  v-for="item in tr.issueTypes"
                   :key="item.id"
                   :label="item.name"
                   :value="item.id"
@@ -42,40 +71,94 @@
               </vs-select>
             </vs-td>
             <vs-td>
-              2
-              <!-- <vs-select filter placeholder="Select" v-model="tr.issueTypeId">
+              <vs-input
+                @change="summaryChange(tr)"
+                v-model="tr.summary"
+                placeholder="请填写"
+              />
+            </vs-td>
+            <vs-td>
+              <vs-select
+                @change="categoryChange(tr, i)"
+                placeholder="请选择"
+                style="width: 200px"
+                v-model="tr.categoryId"
+              >
                 <vs-option
-                  v-for="item in curIssueTypes"
+                  :key="item.id"
+                  v-for="item in issueData.categorys"
+                  :label="item.name"
+                  :value="item.id"
+                >
+                  {{ item.name }}
+                </vs-option>
+              </vs-select>
+            </vs-td>
+            <vs-td>
+              <vs-select
+                @change="subChange(tr, i)"
+                v-if="tr.subSelectIsShow"
+                placeholder="请选择"
+                style="width: 200px"
+                v-model="tr.subCategoryId"
+                :disabled="!tr.subCategorys"
+              >
+                <vs-option
+                  v-for="item in tr.subCategorys"
                   :key="item.id"
                   :label="item.name"
                   :value="item.id"
                 >
                   {{ item.name }}
                 </vs-option>
-              </vs-select> -->
-            </vs-td>
-            <vs-td> 3</vs-td>
-            <vs-td> 4</vs-td>
-            <vs-td> 5</vs-td>
-            <vs-td> 6</vs-td>
-            <vs-td> 7</vs-td>
+              </vs-select></vs-td
+            >
             <vs-td>
-              <vs-button icon flat color="danger">
+              <vs-input
+                type="date"
+                style="width: 150px"
+                v-model="tr.logDate"
+                placeholder="请选择"
+              />
+            </vs-td>
+            <vs-td>
+              <vs-input
+                type="number"
+                :min="1"
+                @change="hoursChange(tr)"
+                v-model="tr.hours"
+                placeholder="请填写"
+              ></vs-input>
+            </vs-td>
+            <vs-td>
+              <vs-button
+                :disabled="workLogData.length === 1"
+                icon
+                flat
+                color="danger"
+                @click="delWorklogData(i)"
+              >
                 <i class="bx bxs-x-square"></i>
               </vs-button>
             </vs-td>
           </vs-tr>
         </template>
       </vs-table>
-      <div>
+      <div style="margin-top: 10px">
         <vs-button
           icon
           flat
+          @click="addWorkLogData"
           color="success"
           style="display: block; margin: 0 auto"
         >
           <i class="bx bx-add-to-queue"></i>
         </vs-button>
+      </div>
+      <div style="margin-top: 10px">
+        <vs-button style="display: block; margin: 0 auto" @click="submit"
+          >提交</vs-button
+        >
       </div>
     </div>
     <vs-dialog
@@ -117,12 +200,29 @@
         </div>
       </template>
     </vs-dialog>
+    <vs-dialog blur width="550px" not-center v-model="confirmActive">
+      <template #header>
+        <h4>确认提交？</h4>
+      </template>
+
+      <div>
+        <p>提交后，你填写的数据将会上传到Jira服务器，请谨慎操作！</p>
+      </div>
+      <template #footer>
+        <div class="con-footer">
+          <vs-button @click="confirm" transparent> 确认 </vs-button>
+          <vs-button @click="confirmActive = false" dark transparent>
+            取消
+          </vs-button>
+        </div>
+      </template>
+    </vs-dialog>
   </section>
 </template>
 
 <script>
-import { getIssueInfo } from "../api/apis";
-import { Base64 } from "../utils/tools";
+import { getIssueInfo, createIssueLog } from "../api/apis";
+import { Base64, format } from "../utils/tools";
 export default {
   name: "Home",
   data() {
@@ -135,13 +235,17 @@ export default {
         projects: [],
       },
       localUser: false,
-      workLogData: [
-        {
-          projectId: "",
-        },
-      ],
-      curIssueTypes: [],
-      aa: "",
+      workLogData: [],
+      projects: [],
+      defaultWorkLog: {
+        projectId: "",
+        issueTypeId: "",
+        categoryId: "",
+        subCategoryId: "",
+        typeSelectIsShow: true,
+        subSelectIsShow: true,
+      },
+      confirmActive: false,
     };
   },
   methods: {
@@ -173,6 +277,8 @@ export default {
                 password: params.password,
               })
             );
+            this.workLogData.push(this.newWorkLog());
+            this.reload(0);
           })
           .catch((err) => {
             this.loginLoading = false;
@@ -181,6 +287,176 @@ export default {
             }
           });
       }
+    },
+    projectChange(tr, i, val) {
+      this.workLogData[i].issueTypeId = "";
+      this.workLogData[i].issueTypes = this.issueData.issueTypes[tr.projectId];
+      this.workLogData[i].typeSelectIsShow = false;
+      this.$nextTick(() => {
+        this.workLogData[i].typeSelectIsShow = true;
+        localStorage.setItem("projectId", tr.projectId);
+        if (val) {
+          this.workLogData[i].issueTypeId = val;
+        }
+      });
+    },
+    issueTypeChange(tr, i) {
+      localStorage.setItem(`${tr.projectId}_issueTypeId`, tr.issueTypeId);
+    },
+    categoryChange(tr, i, val) {
+      this.workLogData[i].subCategoryId = "";
+      this.workLogData[i].subCategorys = this.issueData.subCategorys[
+        tr.categoryId
+      ];
+      this.workLogData[i].subSelectIsShow = false;
+      this.$nextTick(() => {
+        this.workLogData[i].subSelectIsShow = true;
+        localStorage.setItem("categoryId", tr.categoryId);
+        if (val) {
+          this.workLogData[i].subCategoryId = val;
+        }
+      });
+    },
+    subChange(tr, i) {
+      localStorage.setItem(`${tr.categoryId}_subCategoryId`, tr.subCategoryId);
+    },
+    hoursChange(tr) {
+      if (tr.hours) {
+        localStorage.setItem("hours", tr.hours + "");
+      }
+    },
+    summaryChange(tr) {
+      if (tr.summary) {
+        localStorage.setItem("summary", tr.summary);
+      }
+    },
+    addWorkLogData() {
+      this.workLogData.push(this.newWorkLog());
+      let idx = this.workLogData.length - 1;
+      this.reload(idx);
+    },
+    delWorklogData(i) {
+      this.workLogData.splice(i, 1);
+    },
+    newWorkLog() {
+      let newWorkLog = {};
+      Object.assign(newWorkLog, this.defaultWorkLog);
+      newWorkLog.logDate = format(new Date());
+      newWorkLog.projectId = this.getLocalCache("projectId");
+      newWorkLog.issueTypeId = this.getLocalCache(
+        `${newWorkLog.projectId}_issueTypeId`
+      );
+      newWorkLog.categoryId = this.getLocalCache("categoryId");
+      newWorkLog.subCategoryId = this.getLocalCache(
+        `${newWorkLog.categoryId}_subCategoryId`
+      );
+      newWorkLog.hours = this.getLocalCache("hours")
+        ? parseInt(this.getLocalCache("hours"))
+        : 4;
+      newWorkLog.summary = this.getLocalCache("summary");
+      return newWorkLog;
+    },
+    getLocalCache(name) {
+      let val = localStorage.getItem(name);
+      return val || "";
+    },
+    reload(idx) {
+      this.projectChange(
+        this.workLogData[idx],
+        idx,
+        this.workLogData[idx].issueTypeId
+      );
+      this.categoryChange(
+        this.workLogData[idx],
+        idx,
+        this.workLogData[idx].subCategoryId
+      );
+    },
+    submit() {
+      // check data
+      if (this.dataCheck()) {
+        // submit
+        this.confirmActive = true;
+      }
+    },
+    dataCheck() {
+      let i = 1;
+      for (let item of this.workLogData) {
+        let text = `第${i++}条数据,`;
+        if (!item.projectId) {
+          text += "项目未选择";
+          this.dataErr(text);
+          return false;
+        } else if (!item.issueTypeId) {
+          text += "问题类型未选择";
+          this.dataErr(text);
+          return false;
+        } else if (!item.summary) {
+          text += "标题未填写";
+          this.dataErr(text);
+          return false;
+        } else if (!item.categoryId) {
+          text += "分类未选择";
+          this.dataErr(text);
+          return false;
+        } else if (!item.subCategoryId) {
+          text += "子分类未选择";
+          this.dataErr(text);
+          return false;
+        } else if (!item.logDate) {
+          text += "日期未选择";
+          this.dataErr(text);
+          return false;
+        } else if (!item.hours) {
+          text += "工时未填写";
+          this.dataErr(text);
+          return false;
+        }
+      }
+      return true;
+    },
+    dataErr(text) {
+      this.$vs.notification({
+        progress: "auto",
+        position: "top-center",
+        color: "danger",
+        title: "数据填写错误！",
+        text: text,
+      });
+    },
+    confirm() {
+      let params = {
+        loginName: this.user.loginName,
+        password: this.user.password,
+        works: [],
+      };
+      for (let item of this.workLogData) {
+        params.works.push({
+          projectId: item.projectId,
+          issueTypeId: item.issueTypeId,
+          atlToken: this.issueData.atlToken,
+          formToken: this.issueData.formToken,
+          summary: item.summary,
+          categoryId: item.categoryId,
+          subCategoryId: item.subCategoryId,
+          assignee: this.issueData.assignee,
+          logDate: item.logDate,
+          hours: item.hours,
+        });
+      }
+      const loading = this.$vs.loading({
+        text: "啪一下，很快啊！就提交了...",
+      });
+      createIssueLog(params)
+        .then((data) => {
+          this.$vs.notification({
+            progress: "auto",
+            position: "top-center",
+            color: "success",
+            title: "工时提交成功！",
+          });
+        })
+        .catch((data) => {});
     },
   },
   mounted() {
@@ -201,8 +477,7 @@ export default {
 .home {
   .content {
     margin: 80px;
-    height: 900px;
-    max-width: 1200px;
+    max-width: 1650px;
     margin-left: auto;
     margin-right: auto;
     left: 0;
@@ -223,6 +498,20 @@ export default {
     width: calc(100%);
     .vs-input {
       width: 100%;
+    }
+  }
+}
+.con-footer {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  .vs-button {
+    margin: 0;
+    .vs-button__content {
+      padding: 10px 30px;
+    }
+    ~ .vs-button {
+      margin-left: 10px;
     }
   }
 }
